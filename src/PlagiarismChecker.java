098ef3573bb70e8c889aa605831f29f00c8cdb0d
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2025-02-25 11:29:31 -0800

    code

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -16,14 +16,19 @@ public class PlagiarismChecker {
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
         //this function returns the length of the longest shared substring.
-        String substring1;
-        String substring2;
+        String substring1 = "";
+        String substring2 = "";
+        int currentSubstringLength = 0;
+        int longestSubstringLength = 0;
         for(int i = 0; i < doc1.length(); i++){
             for(int j = 0; j < doc2.length(); j++){
                 substring1 += doc1.charAt(i);
-                substring2 +=doc2.charAt(j);
-                if(substring1 + doc1.charAt(i + 1) != substring2 + doc2.charAt(j + 1)){
-                    substring1
+                substring2 += doc2.charAt(j);
+                if(substring1 + doc1.charAt(i + 1) == substring2 + doc2.charAt(j + 1)){
+                    currentSubstringLength++;
+                    if(currentSubstringLength > longestSubstringLength){
+                        longestSubstringLength = currentSubstringLength;
+                    }
                 }
             }
         }
