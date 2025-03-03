abfa55c76c63d58c34bea71b976a75c7727cb34f
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2025-03-02 22:32:44 -0800

    code

---
+++ src/MatchedStr.java
@@ -0,0 +1,5 @@
+public class MatchedStr {
+    public int doc1Index;
+    public int doc2Index;
+    public char matchedChar;
+}
--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Plagiarism Checker
  * A tool for finding the longest shared substring between two documents.
@@ -16,46 +18,53 @@ public class PlagiarismChecker {
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
         //This function returns the length of the longest shared substring.
+        ArrayList<MatchedStr> matchedStrings = new ArrayList<MatchedStr>();
+        /*
+        //This uses memoisation
         int[][] memArr = new int[doc1.length() + 1][doc2.length() + 1];
         for(int i = 0; i < doc1.length()+1; i++){
             for(int j= 0; j < doc2.length()+1; j++) {
-                memArr[i][j] = -1;
+                if(i == 0 || j == 0) {
+                    memArr[i][j] = 0;
+                } else {
+                    memArr[i][j] = -1;
+                }
             }
         }
-        int[][] tabArr = new int[doc1.length() + 1][doc2.length() + 1];
-        return LCS(doc1.length(), doc2.length(), doc1, doc2, memArr);
+        int count =  LCS(doc1.length(), doc2.length(), doc1, doc2, memArr, matchedStrings);
+        printLongestSubString(matchedStrings,true,count);
+        return count;
+        */
 
-        /*
-            Below I have done tabulation.
+
+
+
+            //Below I have done tabulation.
+        int[][] tabArr = new int[doc1.length() + 1][doc2.length() + 1];
 
         for (int i = 0; i < tabArr.length; i++) {
             for (int j = 0; j < tabArr[0].length; j++) {
-                tabArr[i][j] = tabulation(i, j, doc1, doc2, tabArr);
+                tabArr[i][j] = tabulation(i, j, doc1, doc2, tabArr, matchedStrings);
             }
         }
-        //return tabArr[doc1.length()][doc2.length()];
-
-         */
+        printLongestSubString(matchedStrings,true, tabArr[doc1.length()][doc2.length()]);
+        return tabArr[doc1.length()][doc2.length()];
     }
 
-    public static int LCS(int i, int j, String doc1, String doc2, int[][] memArr){
+    public static int LCS(int i, int j, String doc1, String doc2, int[][] memArr, ArrayList<MatchedStr> matchedStrings){
         if(i < 0 || i > doc1.length() || j < 0 || j > doc2.length()) {
             return 0;
         }
-        if(i == 0){
-            memArr[i][j] = 0;
-            return 0;
-        }
-        if (j == 0){
-            memArr[i][j] = 0;
-            return 0;
+        if((i == 0) || (j == 0)) {
+            return memArr[i][j];
         }
         if(doc1.charAt(i - 1) == doc2.charAt(j - 1)){
+            addCharToLongestSubString(i,j, doc1,matchedStrings);
             if(memArr[i-1][j-1] != -1) {
                 memArr[i][j] = memArr[i-1][j-1]+1;
             }
             else {
-                memArr[i][j] = LCS(i-1, j-1, doc1, doc2, memArr) + 1;
+                memArr[i][j] = LCS(i-1, j-1, doc1, doc2, memArr, matchedStrings) + 1;
             }
         }
         else{
@@ -64,27 +73,59 @@ public class PlagiarismChecker {
             if(memArr[i-1][j] != -1) {
                 m1 = memArr[i-1][j];
             } else {
-                m1 = LCS(i-1, j, doc1, doc2, memArr);
+                m1 = LCS(i-1, j, doc1, doc2, memArr, matchedStrings);
             }
             if(memArr[i][j-1] != -1) {
                 m2 = memArr[i][j-1];
             } else {
-                m2 = LCS(i, j-1, doc1, doc2, memArr);
+                m2 = LCS(i, j-1, doc1, doc2, memArr, matchedStrings);
             }
             memArr[i][j] = Math.max(m1, m2);
         }
         return memArr[i][j];
     }
 
-    public static int tabulation(int i, int j, String doc1, String doc2, int[][] tabArr){
+    public static int tabulation(int i, int j, String doc1, String doc2, int[][] tabArr, ArrayList<MatchedStr> matchedStrings){
         if((i == 0) || (j == 0)){
             return 0;
         }
         if(doc1.charAt(i - 1) == doc2.charAt(j - 1)){
+            addCharToLongestSubString(i, j, doc1, matchedStrings);
             return tabArr[i - 1][j - 1] + 1;
         }
         else{
             return Math.max(tabArr[i - 1][j], tabArr[i][j - 1]);
+        }
+    }
+    public static void addCharToLongestSubString(int i, int j, String doc1, ArrayList<MatchedStr> matchedStrings) {
+            MatchedStr tStr = new MatchedStr();
+            tStr.matchedChar = new char[doc1.length()];
+            tStr.doc1Index = i;
+            tStr.doc2Index = j;
+            tStr.matchedChar = doc1.charAt(i-1);
+            matchedStrings.add(tStr);
+            return;
+    }
+    public static void printLongestSubString(ArrayList<MatchedStr> resultsArr, boolean allFlag, int count) {
+        MatchedStr resultStr;
+        if(resultsArr.size() <= 0) {
+            System.out.println("No matching string");
+            return;
+        }
+        int Len = 0;
+        Len = resultsArr.size()+1;
+        if(!allFlag) {
+            resultStr = resultsArr.get(0);
+            resultStr.matchedChar[resultStr.matchedStrLen] = '\0';
+            System.out.println("One Matched String: "+resultStr.matchedChar);
+        }
+        else {
+            System.out.println("All Matched Strings:");
+            for(int k = 0; k < resultsArr.size(); k++) {
+                resultStr = resultsArr.get(k);
+                resultStr.matchedChar[resultStr.matchedStrLen] = '\0';
+                System.out.println(resultStr.matchedChar);
+            }
         }
     }
 }
